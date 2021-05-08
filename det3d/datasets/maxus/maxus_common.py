f033531407d22ee5a73b18c9242e714110ced698
Sean Zhang <sean_zhang777@163.com>
2021-05-08 09:17:14 +0800

    update empty_result_anno to fix eval bug

--- det3d/datasets/maxus/maxus_common.py
+++ det3d/datasets/maxus/maxus_common.py
@@ -725,13 +725,10 @@ def empty_result_anno():
     annotations.update(
         {
             "name": np.array([]),
-            "truncated": np.array([]),
             "occluded": np.array([]),
-            "alpha": np.array([]),
             "bbox": np.zeros([0, 4]),
-            "dimensions": np.zeros([0, 3]),
-            "location": np.zeros([0, 3]),
-            "rotation_y": np.array([]),
+            "box3d_camera": np.zeros([0, 7]),
+            "box3d_lidar": np.zeros([0, 7]),
             "score": np.array([]),
         }
     )
